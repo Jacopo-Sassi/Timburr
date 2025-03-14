1a0b53df31ead5c28ee948673015130549abcada
Jacopo-Sassi <sassijacopp@gmail.com>
2025-03-14 09:43:35 +0100

    Organized better the description of Timburr

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,9 @@ function Main () {
                 <h3 className="info-title"> Timburr (Pokemon)</h3>
                 <p className="description">Timburr (Japanese: ドッコラー Dokkorer) is a Fighting-type Pokémon introduced in Generation V. It evolves into Gurdurr starting at level 25, which evolves into Conkeldurr when traded.</p>
                 <h5>Biology</h5>
-                <p>Timburr is a gray bipedal Pokémon with a dark purple nose and dark-gray oval-shaped eyes. It has bulbous protrusions extending from the front and lower parts of its head. Pink, vein-like bulges are present on the back of its head. Similar pink bands adorn its shoulders and thighs. It always carries squared logs and uses them to fight with. When it can easily lift a block of timber nearly three times its own weight, it is close to evolving. Timburr is usually found near construction sites, helping construction workers. It loves helping in construction so much that if rain causes work to cease, it will throw a tantrum while swinging its log.</p>
+                <p>Timburr is a gray bipedal Pokémon with a dark purple nose and dark-gray oval-shaped eyes. Pink, vein-like bulges are present on the back of its head. Similar pink bands adorn its shoulders and thighs.</p>
+                <p>It has bulbous protrusions extending from the front and lower parts of its head. It always carries squared logs and uses them to fight with. When it can easily lift a block of timber nearly three times its own weight, it is close to evolving.</p>
+                <p>Timburr is usually found near construction sites, helping construction workers. It loves helping in construction so much that if rain causes work to cease, it will throw a tantrum while swinging its log. </p>
             </div>
         </div>
     )
